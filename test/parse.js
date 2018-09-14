a037e972fcbc0bb6e089d29754497a168ce76c92
stur86 <simonesturniolo@gmail.com>
2018-09-14 13:54:28 +0100

    Implemented block parsing

--- test/parse.js
+++ test/parse.js
@@ -1,10 +1,30 @@
 'use strict';
 
 var fs = require('fs');
+var expect = require('chai').expect;
 var tokens = require('../lib/tokens.js');
-var parseCif = require('../lib/parse.js');
+var parser = require('../lib/parse.js');
 
-fs.readFile(__dirname + '/../examples/example_single.cif', 'utf8',
-    function(err, file) {
-        parseCif(file);
+/*
+
+        fs.readFile(__dirname + '/../examples/example_single.cif', 'utf8',
+            function(err, file) {
+                parser.parseCif(file);
+            });        
+ */
+
+describe('#parsing', function() {
+    it('should correctly strip away comments', function() {
+        var test = "# This is a comment \ndata_block # This one too\n";
+        var stripped = parser.stripComments(test);
+        expect(stripped).to.equal("data_block ")
     });
+    it('should find the correct data blocks', function() {
+        var test = "data_block_0\n_item 0\ndata_block_1";
+        var blocks = parser.parseDataBlocks(test);
+        console.log(blocks);
+        expect(blocks[0][0]).to.equal("block_0");
+        expect(blocks[1][0]).to.equal("block_1");
+    });
+
+});
