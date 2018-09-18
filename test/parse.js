4a4f97535a9ae41d247be4590d10baab9d4afc41
stur86 <wml08325@scpc044.esc.rl.ac.uk>
2018-09-18 10:31:54 +0100

    Added test for parseCif

--- test/parse.js
+++ test/parse.js
@@ -5,14 +5,6 @@ var expect = require('chai').expect;
 var tokens = require('../lib/tokens.js');
 var parser = require('../lib/parse.js');
 
-/*
-
-        fs.readFile(__dirname + '/../examples/example_single.cif', 'utf8',
-            function(err, file) {
-                parser.parseCif(file);
-            });        
- */
-
 describe('#parsing', function() {
     it('should correctly strip away comments', function() {
         var test = "# This is a comment \ndata_block # This one too\n";
@@ -65,31 +57,18 @@ describe('#parsing', function() {
         expect(items[3].value[1].num).to.equal(3);
         expect(items[4].value[0].num).to.equal(2);
     });
-    /*
-    it('should correctly identify data items', function() {
-        var test = "_a 5\n_b 1.2e3\n_c 4.5(34)\n_d 'Word'\n_e ;\nParagraph\n;";
-        var items = parser.parseDataItems(test);
-        for (var i = 0; i < 5; ++i) {
-            expect(items[i].tag).to.equal(['_a', '_b', '_c', '_d', '_e'][i]);
-            if (i < 3) {
-                expect(items[i].value.num).to.equal([5, 1200, 4.5][i]);
-            } else {
-                expect(items[i].value.text).to
-                    .equal(['Word', '\nParagraph\n'][i - 3]);
-            }
-        }
-        var test = "loop_\n_a\n_b\n0\n;\nTest\n;\n1\n'Test 2'";
-        var items = parser.parseDataItems(test);
-        expect(items[0].tag).to.equal('_a');
-        expect(items[1].tag).to.equal('_b');
-        expect(items[0].type).to.equal('loop');
-        expect(items[1].type).to.equal('loop');
-        expect(items[0].value[0].num).to.equal(0);
-        console.log(items[1].value[0]);
-        console.log(items[0].value[1]);
-        console.log(items[1].value[1]);
-        expect(items[1].value[1].text).to.equal('Test 2');
-    })
-    */
+    it('should parse a whole file', function() {
+        var contents = fs.readFileSync(__dirname +
+            '/../examples/example_single.cif',
+            'utf8');
 
+        var cifdict = parser.parseCif(contents);
+
+        expect(cifdict).to.have.property('global');
+        expect(cifdict).to.have.property('I');
+
+        expect(cifdict.global._publ_body_element.type).to.equal('loop');
+        expect(cifdict.I._chemical_formula_moiety.value.text).to.equal(
+            'C19 H19 N O3');
+    });
 });
