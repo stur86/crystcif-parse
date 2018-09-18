f1bbea70195277eba0921bbb36a412f8f05287a7
stur86 <wml08325@scpc044.esc.rl.ac.uk>
2018-09-18 09:57:18 +0100

    Completed basic CIF parsing

--- index.js
+++ index.js
@@ -16,12 +16,10 @@ var tokens = require('./lib/tokens.js');
 //console.log(tokens.tokenRegex('chrstring').exec("'C'  'C'   0.0033   0.0016"));
 //console.log(tokens.tokenRegex('loop_body').exec("C1 C 0.6424(3) 0.5942(2) 0.7939(3) 0.0186(5) Uani 1 1 d . . . "));
 
-fs.readFile(__dirname + '/examples/simple_loop.cif', 'utf8',
+fs.readFile(__dirname + '/examples/example_single.cif', 'utf8',
     function(err, file) {
-        var tk = parser.tokenize(file);
-        console.log(tk);
-        var db = parser.parseDataBlocks(tk);
-        console.log(db);
-        var ditems = parser.parseDataItems(db[0][1]);
-        console.log(ditems);
+        var cd = parser.parseCif(file);
+        for (var k in cd.I) {
+            console.log(k);            
+        }
     }); 
