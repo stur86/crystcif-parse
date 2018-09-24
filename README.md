a0640238bebeb548c1d0bd99bca9618d94c02742
stur86 <wml08325@scpc044.esc.rl.ac.uk>
2018-09-24 15:08:45 +0100

    Cleaning up; updating README

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # crystcif-parse
-A JavaScript parser for Crystallography CIF files
+
+A JavaScript parser for Crystallography CIF files. This module provides a
+barebone structure to parse the CIF data format and interpret some basic
+keywords in order to retrieve a crystal structure.
+
+### Supported:
+* parsing of CIF 1.1 syntax
+* partial interpretation of structural core dictionary keywords (position, cell
+  parameters, labels)
+* basic treatment of symmetry (symmetry operations)
+
+### Not supported yet:
+* CIF 2.0 syntax
+* spacegroup symbols
+* non-essential atomic properties (masses, charges, bonds, etc.)
+
+
--- index.js
+++ index.js
@@ -1,39 +1,20 @@
 'use strict';
 
-/**
- * Parses a cif file in string format, returning it as a data structure
- * @param  {string} ciftext CIF file as a string
- * @return {Object}
- */
-module.exports = function crystCifParse(ciftext) {
-
-}
 
 var fs = require('fs');
 var parser = require('./lib/parse.js');
 var tokens = require('./lib/tokens.js');
 var Atoms = require('./lib/cryst.js').Atoms;
 
-//console.log(tokens.tokenRegex('chrstring').exec("'C'  'C'   0.0033   0.0016"));
-//console.log(tokens.tokenRegex('loop_body').exec("C1 C 0.6424(3) 0.5942(2) 0.7939(3) 0.0186(5) Uani 1 1 d . . . "));
+/**
+ * Parses a cif file in string format, returning it as a data structure
+ * @param  {string} ciftext CIF file as a string
+ * @return {Object}
+ */
+module.exports = function crystCifParse(ciftext) {
+    return Atoms.readCif(ciftext);
+}
 
-fs.readFile(process.argv[2], 'utf8',
-    function(err, file) {
-        var a = Atoms.readCif(file);
-
-        // Print it out as cell
-        console.log('%block lattice_cart');
-        var c = a.I.get_cell();
-        for (var i = 0; i < 3; ++i) {
-            console.log(c[i].join(' '));
-        }
-        console.log('%endblock lattice_cart');
-        var syms = a.I.get_chemical_symbols();
-        var pos = a.I.get_positions();
-        console.log('%block positions_abs');
-        for (var i = 0; i < a.I.length(); ++i) {
-            console.log(syms[i] + ' ' + pos[i].join(' '));
-        }
-        console.log('%endblock positions_abs');
-
-    });
+// Additional exports
+module.exports.Atoms = Atoms;
+module.exports.parseCif = parser.parseCif;
