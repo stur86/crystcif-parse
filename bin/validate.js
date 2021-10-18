7d1bccbda1a86f93c868707245103de8810795d2
stur86 <simonesturniolo@gmail.com>
2021-10-18 12:29:55 +0100

    Fixed problem with validate.js script

--- bin/validate.js
+++ bin/validate.js
@@ -2,19 +2,23 @@
 
 'use strict';
 
+var fs = require('fs');
 var Atoms = require('../lib/cryst.js').Atoms;
 
 var files = process.argv.slice(2);
 
 for (var i = 0; i < files.length; ++i) {
     var f = files[i];
-    var err = null;
-    try {
-        var a = Atoms.readCif(f);
-    } catch (emsg) {
-        console.log('CIF file ' + f + ' failed to parse: ' + emsg);
-    }
-    if (!err) {
-        console.log('CIF file ' + f + ' parsed successfully!');
-    }
+    // Read in file contents
+    var fc = fs.readFile(f, 'utf8', (err, data) => {
+        try {
+            var a = Atoms.readCif(data);
+        } catch (emsg) {
+            console.log('CIF file ' + f + ' failed to parse: ' + emsg);
+            err = emsg;
+        }
+        if (!err) {
+            console.log('CIF file ' + f + ' parsed successfully!');
+        }        
+    });
 }
